9d77876258f21e2031f61e06421cc4d148189a4b
Leo <leozhaunr@yahoo.com.br>
2022-05-27 10:48:32 -0300

    create PostCategories tests

---
+++ tests/TestCase/Controller/PostCategoriesControllerTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\PostCategoriesController;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\PostCategoriesController Test Case
+ *
+ * @uses \App\Controller\PostCategoriesController
+ */
+class PostCategoriesControllerTest extends TestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.PostCategories',
+        'app.Posts',
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     */
+    public function testAdd()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEdit()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
